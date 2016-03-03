c2c7a0a216c42e4bf8f3f9c6fbe28a4c84d49e84
Scott Johnson <baronvonvaderham@gmail.com>
2016-03-03 16:52:14 -0500

    Version Bump to 0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='django-integration-settings',
-    version='0.0.2',
+    version='0.0.3',
     description="Reusable settings templates for tools we commonly integrate with.",
     author='AASHE',
     author_email='it@aashe.org',
